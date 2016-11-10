309ceafc2594ca961cab6ba167583fe6e19091a4
Tyler Lazoen <tylerlazoen@gmail.com>
2016-11-10 04:58:16 -0700

    Modify text processing to only return nouns

--- text_processing.py
+++ text_processing.py
@@ -20,12 +20,6 @@ to perform topic modeling
 '''
 
 
-# Global variables
-STOPLIST = set(stopwords.words('english') + ["n't", "'s", "'m", "'", "'re", "'ve"] \
-           + ['philosophy', 'philosophers'] + list(ENGLISH_STOP_WORDS)) + \
-           list([ch for ch in 'abcdefghijklmnopqrstuvwxyz'])
-
-
 def clean_document(doc):
     '''
     INPUT:
@@ -37,7 +31,7 @@ def clean_document(doc):
     Cleans a single document's text
     '''
     # Part's of speech to keep in the result
-    pos_lst = ['ADJ', 'ADV', 'NOUN', 'PROPN', 'VERB', 'AUX']
+    pos_lst = ['NOUN']
     tokens = [token.lemma_.lower().strip(punctuation).strip().replace(' ', '_') for token in doc if token.pos_ in pos_lst]
 
     spaces_lst = ['', ' ', '\n', '\n\n', '\r']
@@ -46,14 +40,31 @@ def clean_document(doc):
     tokens = [token for token in tokens if token not in STOPLIST]
 
     clean_text = ' '.join(token for token in tokens)
-
     return clean_text, tokens
 
 
-def cleanse_corpus(documents):
+def split_text(doc, title, n_words):
+    words = str(doc).split()
+    chunks = []
+    current_chunk_words = []
+    current_chunk_word_count = 0
+    for word in words:
+        current_chunk_words.append(word)
+        current_chunk_word_count += 1
+        if current_chunk_word_count == n_words:
+            chunks.append(' '.join(current_chunk_words))
+            current_chunk_words = []
+            current_chunk_word_count = 0
+    chunks.append(' '.join(current_chunk_words))
+
+    return chunks, [title] * len(chunks)
+
+
+def cleanse_corpus(documents, docs):
     '''
     INPUT:
         documents - an array-like object of text documents
+        docs - documents dataframe
     OUTPUT:
         cleaned_texts - an array of documents cleaned of
                         unnecessary words and characters
@@ -62,7 +73,7 @@ def cleanse_corpus(documents):
 
     Cleanses entire corpus of documents using SpaCy
     '''
-    parsed_docs = []
+    titles_chunks = []
     tokenized_docs = []
 
     num_cores = multiprocessing.cpu_count()
@@ -71,16 +82,29 @@ def cleanse_corpus(documents):
         batch_size = 1
 
     print("{} Documents Total with {} Cores Working".format(len(documents), num_cores-1))
-    num_doc = 1
+    num_doc = 0
     for doc in parser.pipe(documents, batch_size=batch_size, n_threads=num_cores - 1):
-        print("\nCleaning Text for Document {}".format(num_doc))
-        cleaned_text, tokens = clean_document(doc)
-        doc = parser(cleaned_text)
-        parsed_docs.append(doc)
-        tokenized_docs.append(tokens)
+        print("\nCleaning Text for Document {}".format(num_doc+1))
+        title = docs.df.loc[num_doc, 'title']
+        if len(doc) > 1000:
+            print("Chunking Document...")
+            chunks, titles = split_text(doc, title, n_words=1000)
+            titles_chunks.extend(titles)
+            for chunk in chunks:
+                doc_chunk = parser(chunk)
+                cleaned_text, tokens = clean_document(doc_chunk)
+                tokenized_docs.append(tokens)
+            print("Tokenized docs: {}, Titles: {}".format(len(tokenized_docs), len(titles_chunks)))
+        else:
+            print("Document less than 1000 words")
+            cleaned_text, tokens = clean_document(doc)
+            tokenized_docs.append(tokens)
+            titles_chunks.append(title)
+            print("Tokenized docs: {}, Titles: {}".format(len(tokenized_docs), len(titles_chunks)))
+
         num_doc += 1
 
-    return parsed_docs, tokenized_docs
+    return np.array(tokenized_docs), np.array(titles_chunks)
 
 
 def load_data():
@@ -109,11 +133,22 @@ if __name__ == '__main__':
     print('Downloading Parser...')
     parser = English()
 
-    parsed_docs, tokenized_docs = cleanse_corpus(full_texts)
+    STOPLIST = set(stopwords.words('english') + ["n't", "'s", "'m", "'", "'re", "'ve"] + \
+                   ['philosophy', 'philosophers', 'philosopher', 'source', 'translator', \
+                    'editor', 'tranlation', 'publication', 'version', 'material', 'mennen'] + \
+                   [word.strip() for name in phils.df.name.values for word in name.split()] + \
+                   [word.strip() for title in docs.df.title.values for word in title.split()] + \
+                   list(ENGLISH_STOP_WORDS) + list([ch for ch in 'abcdefghijklmnopqrstuvwxyz']))
+
+    tokenized_docs, all_titles = cleanse_corpus(full_texts, docs)
     print('Finished!  Beginning Next Process...')
 
-    print('\nPickling objects...')
-    with open('data/model/tokenized_docs.pkl', 'wb') as f:
-        pickle.dump(tokenized_docs, f)
-    with open('data/model/parsed_docs.pkl', 'wb') as f:
-        pickle.dump(parsed_docs, f)
+    if len(tokenized_docs) == len(all_titles):
+        print('\nPickling objects...')
+        with open('data/model/tokenized_docs.pkl', 'wb') as f:
+            pickle.dump(tokenized_docs, f)
+        with open('data/model/chunk_titles.pkl', 'wb') as f:
+            pickle.dump(all_titles, f)
+    else:
+        print(len(tokenized_docs), len(all_titles))
+        print("Lengths don't match!")
